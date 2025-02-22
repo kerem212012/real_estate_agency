1c929301f430fb3bf1321dd2c5c6359fffc75de0
kerem212012 <rockstarfoxykerem@gmail.com>
2025-02-22 18:11:37 +0300

    add db model for complaints

--- property/models.py
+++ property/models.py
@@ -1,6 +1,8 @@
+from tkinter.constants import CASCADE
+
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
@@ -50,3 +52,11 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+class Complaint(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,verbose_name="Кто жаловался")
+    flat = models.ForeignKey(Flat,on_delete=models.CASCADE,verbose_name="Квартира, на которую пожаловались")
+    text = models.TextField(verbose_name="Текст жалобы")
+
+    def __str__(self):
+        return f'Complaint from {self.user} to {self.flat}'
